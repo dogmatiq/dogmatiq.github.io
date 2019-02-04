7adf30a09ec10a1190eea6468d09073b6580faa2
James Harris <mailjamesharris@gmail.com>
2019-02-04 12:42:11 +1000

    Split repo list into sections and dynamically build the contributor list.

--- res/js/github.js
+++ res/js/github.js
@@ -3,7 +3,25 @@
 var github = new GitHub({token: '20c7969d26605b4d2de4bafc486e2637ec482930'});
 
 github.get('orgs/dogmatiq/repos?type=public', null, function(err, repositories) {
-    var repos = [];
+    var dogma = {
+        title: "Dogma",
+        desc: "A suite of tools for developing message-based distributed applications in Go.",
+        repos: [],
+    }
+
+    var engines = {
+        title: "Dogma Engines",
+        desc: "Dogma engine implementations and utilities for engine developers.",
+        repos: [],
+    }
+
+    var libraries = {
+        title: "Libraries",
+        desc: "Standalone Go modules that were developed in support of other Dogmatiq projects.",
+        repos: [],
+    }
+
+    var categories = [dogma, engines, libraries]
 
     if (err) {
         console.log(err)
@@ -11,50 +29,65 @@ github.get('orgs/dogmatiq/repos?type=public', null, function(err, repositories) 
         for (var i = 0; i < repositories.length; ++i) {
             var repo = repositories[i];
 
-            if (!repo.has_issues) {
-                continue;
+            var r = {
+                name: repo.name,
+                desc: repo.description,
+                url: repo.html_url,
             }
 
             if (repo.language == "Go") {
-                repo.go_doc_url = "https://godoc.org/github.com/"+repo.full_name
+                r.godoc = "https://godoc.org/github.com/"+repo.full_name
             }
 
-            if (repo.has_pages) {
-                repo.primary_link_url = repo.name + "/"
-            } else if (repo.go_doc_url) {
-                repo.primary_link_url = repo.go_doc_url
-            } else {
-                repo.primary_link_url = repo.http_url
+            if (repo.name.match(/\bengine\b/i) || repo.description.match(/\bengine\b/i)) {
+                engines.repos.push(r)
+            } else if (repo.name.match(/\bdogma\b/i) || repo.description.match(/\bdogma\b/i)) {
+                dogma.repos.push(r)
+            } else if (repo.language == "Go") {
+                libraries.repos.push(r)
             }
-
-            repos.push(repo)
         }
     }
 
     riot.mount(
         'project-list',
         {
-            repositories: repos,
+            categories: categories,
+            err: err,
         },
     );
+})
 
-    riot.mount(
-        'contributor-list',
-        {
-            contributors: [
+github.get('orgs/dogmatiq/members', null, function(err, members) {
+    if (err) {
+        console.log(err)
+        return
+    }
+
+    var contributors = []
+
+    for (var i = 0; i < members.length; ++i) {
+        var member = members[i]
+
+        github.get('users/'+member.login, null, function(err, member) {
+            if (err) {
+                console.log(err)
+                return
+            }
+
+            contributors.push(
                 {
-                    user: "jmalloc",
-                    name: "James Harris",
-                },
+                    user: member.login,
+                    name: member.name,
+                }
+            )
+
+            riot.mount(
+                'contributor-list',
                 {
-                    user: "danilvpetrov",
-                    name: "Danil Petrov",
-                },
-                {
-                    user: "koden-km",
-                    name: "Kevin Millar",
-                },
-            ],
-        },
-    );
+                    contributors: contributors,
+                }
+            )
+        })
+    }
 })
