d5e75f9a42bc2b51e41bcd1b8a46bb9d8be637c8
James Harris <contact@jamesharris.id.au>
2023-03-01 13:26:12 +1000

    Add "deprecated projects" section.

--- res/js/github.js
+++ res/js/github.js
@@ -2,105 +2,116 @@
 
 var github = new GitHub();
 
-github.get('orgs/dogmatiq/repos?type=public', {all: true}, function(err, repositories) {
+github.get(
+  "orgs/dogmatiq/repos?type=public",
+  { all: true },
+  function (err, repositories) {
     var dogma = {
-        id: "dogma",
-        title: "Dogma",
-        desc: "A suite of tools for developing message-based distributed applications in Go.",
-        repos: [],
-    }
+      id: "dogma",
+      title: "Dogma",
+      desc: "A suite of tools for developing message-based distributed applications in Go.",
+      repos: [],
+    };
 
     var engines = {
-        id: "dogma-engines",
-        title: "Dogma Engines",
-        desc: "Dogma engine implementations and utilities for engine developers.",
-        repos: [],
-    }
+      id: "dogma-engines",
+      title: "Dogma Engines",
+      desc: "Dogma engine implementations and utilities for engine developers.",
+      repos: [],
+    };
 
     var libraries = {
-        id: "libraries",
-        title: "Supporting Libraries",
-        desc: "Standalone Go modules that were developed in support of other Dogmatiq projects.",
-        repos: [],
-    }
+      id: "libraries",
+      title: "Supporting Libraries",
+      desc: "Standalone Go modules that were developed in support of other Dogmatiq projects.",
+      repos: [],
+    };
 
-    var categories = [dogma, engines, libraries]
+    var deprecated = {
+      id: "deprecated",
+      title: "Deprecated Projects",
+      desc: "Projects that should no longer be used for new designs but are not yet archived.",
+      repos: [],
+    };
+
+    var categories = [dogma, engines, libraries, deprecated];
 
     if (err) {
-        console.log(err)
+      console.log(err);
     } else {
-        for (var i = 0; i < repositories.length; ++i) {
-            var repo = repositories[i];
+      for (var i = 0; i < repositories.length; ++i) {
+        var repo = repositories[i];
 
-            if (repo.archived) {
-                console.log(repo.name, "ignoring archived repo")
-                continue;
-            }
-
-            if (repo.description.match(/internal/i)) {
-                console.log(repo.name, "ignoring internal repo")
-                continue;
-            }
-
-            var r = {
-                name: repo.name,
-                desc: repo.description,
-                url: repo.html_url+'#readme',
-            }
-
-            if (repo.language == "Go") {
-                r.godoc = "https://pkg.go.dev/github.com/"+repo.full_name
-            }
-
-            if (repo.name.match(/\bengines?\b/i) || repo.description.match(/\bengines?\b/i)) {
-                engines.repos.push(r)
-            } else if (repo.name.match(/\bdogma\b/i) || repo.description.match(/\bdogma\b/i)) {
-                dogma.repos.push(r)
-            } else if (repo.language == "Go") {
-                libraries.repos.push(r)
-            }
+        if (repo.archived) {
+          console.log(repo.name, "ignoring archived repo");
+          continue;
         }
+
+        if (repo.description.match(/internal/i)) {
+          console.log(repo.name, "ignoring internal repo");
+          continue;
+        }
+
+        var r = {
+          name: repo.name,
+          desc: repo.description,
+          url: repo.html_url + "#readme",
+        };
+
+        if (repo.language == "Go") {
+          r.godoc = "https://pkg.go.dev/github.com/" + repo.full_name;
+        }
+
+        if (repo.description.match(/🚫/i)) {
+          deprecated.repos.push(r);
+        } else if (
+          repo.name.match(/\bengines?\b/i) ||
+          repo.description.match(/\bengines?\b/i)
+        ) {
+          engines.repos.push(r);
+        } else if (
+          repo.name.match(/\bdogma\b/i) ||
+          repo.description.match(/\bdogma\b/i)
+        ) {
+          dogma.repos.push(r);
+        } else if (repo.language == "Go") {
+          libraries.repos.push(r);
+        }
+      }
     }
 
-    riot.mount(
-        'project-list',
-        {
-            categories: categories,
-            err: err,
-        },
-    );
-})
+    riot.mount("project-list", {
+      categories: categories,
+      err: err,
+    });
+  }
+);
 
-github.get('orgs/dogmatiq/members', null, function(err, members) {
-    if (err) {
-        console.log(err)
-        return
-    }
+github.get("orgs/dogmatiq/members", null, function (err, members) {
+  if (err) {
+    console.log(err);
+    return;
+  }
 
-    var contributors = []
+  var contributors = [];
 
-    for (var i = 0; i < members.length; ++i) {
-        var member = members[i]
+  for (var i = 0; i < members.length; ++i) {
+    var member = members[i];
 
-        github.get('users/'+member.login, null, function(err, member) {
-            if (err) {
-                console.log(err)
-                return
-            }
+    github.get("users/" + member.login, null, function (err, member) {
+      if (err) {
+        console.log(err);
+        return;
+      }
 
-            contributors.push(
-                {
-                    user: member.login,
-                    name: member.name,
-                }
-            )
+      contributors.push({
+        user: member.login,
+        name: member.name,
+      });
 
-            riot.mount(
-                'contributor-list',
-                {
-                    contributors: contributors,
-                }
-            )
-        })
-    }
-})
+      riot.mount("contributor-list", {
+        contributors: contributors,
+      });
+    });
+  }
+});
